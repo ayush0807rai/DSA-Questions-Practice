acc98b454368f70b1268b068bf0f4460365f5ccc
ayush0807rai <ayush080705@gmail.com>
2025-09-24 20:13:35 +0530

    2^n_seriesSum.java

--- 24sept_ques/2^n_seriesSum.java
+++ 24sept_ques/2^n_seriesSum.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        long sum = (long)Math.pow(2, n + 1) - 1;
+        System.out.println("Sum of series = " + sum);
+    }
+}
 
