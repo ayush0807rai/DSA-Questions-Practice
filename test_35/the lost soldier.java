2f625c5566a506efac22636b4550a84161bd89af
ayush0807rai <ayush080705@gmail.com>
2025-10-07 19:16:00 +0530

    the lost soldier.java

---
+++ test_35/the lost soldier.java
@@ -0,0 +1,13 @@
+public class Q2 {
+    public static int lostSoldier(int n, int[] arr) {
+        int sum = n * (n + 1) / 2;
+        int actual = 0;
+        for (int x : arr)
+            actual += x;
+        return sum - actual;
+    }
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 4, 5};
+        System.out.println(lostSoldier(5, arr));
+    }
+}
