396217091135358c93abff4c42e0128122d28b8f
ayush0807rai <ayush080705@gmail.com>
2025-10-07 19:33:41 +0530

    the dragon's roar.java

---
+++ test_35/the dragon's roar.java
@@ -0,0 +1,10 @@
+public class ques_14 {
+    static void print1toN(int n){
+        if(n==0) return;
+        print1toN(n-1);
+        System.out.print(n+" ");
+    }
+    public static void main(String[] args){
+        print1toN(5);
+    }
+}
